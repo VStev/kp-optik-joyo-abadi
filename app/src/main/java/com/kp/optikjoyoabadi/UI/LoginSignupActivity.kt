7340caa95b566672568879afaa50689d4a83b6e9
Aprilla <markusssandreas@gmail.com>
2021-10-10 23:07:36 +0700

    initial commit

---
+++ app/src/main/java/com/kp/optikjoyoabadi/UI/LoginSignupActivity.kt
@@ -0,0 +1,11 @@
+package com.kp.optikjoyoabadi.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginSignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_signup)
+    }
+}
